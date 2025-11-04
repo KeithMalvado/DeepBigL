e4965baa6472fb9b7ad267aa12e1be248438f2e4
KeithMalvado <keithmalvado@gmail.com>
2025-11-04 07:33:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,10 @@ import streamlit as st
 import tensorflow as tf
 import os
 
-# ID file dari Google Drive
 file_id = "1aBcDxyzEfgHIJkLMn"
 url = f"https://drive.google.com/uc?id={file_id}"
 output = "EfficientNetB6_best_model.h5"
 
-# Cek apakah sudah ada file lokal
 if not os.path.exists(output):
     with st.spinner("Mengunduh model dari Google Drive..."):
         gdown.download(url, output, quiet=False)
@@ -17,3 +15,4 @@ if not os.path.exists(output):
 model = tf.keras.models.load_model(output)
 
 st.success("Model berhasil dimuat!")
+
